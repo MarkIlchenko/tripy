59be21307a860528e7a41272923848cf275050d4
Mark Ilchenko <markilchenko18.hillel@gmail.com>
2024-02-22 15:11:17 +0200

    fixed

--- app/(root)/sanity-post/[id]/page.tsx
+++ app/(root)/sanity-post/[id]/page.tsx
@@ -1,4 +1,5 @@
 import ResourceCard from '@/components/ResourceCard';
+import WeatherComponent from '@/components/WeatherComponent';
 import { getResourcesPlaylist } from '@/sanity/actions';
 import { currentUser } from '@clerk/nextjs';
 
@@ -31,9 +32,25 @@ const page = async ({ params }: { params: { id: string }}) => {
     return <div>Ресурс не найден</div>;
   }
 
+  // Получение текущей даты и даты через 7 дней
+  const today = new Date();
+  const nextWeek = new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000);
+
+  const formatDate = (date: Date): string => {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
+  }
+  
+
+  // Форматирование дат в формате YYYY-MM-DD
+  const formattedToday = formatDate(today);
+  const formattedNextWeek = formatDate(nextWeek);
+
   // Отображение выбранного ресурса
   return (
-    <div>
+    <div className="flex-center paddings mx-auto w-full max-w-screen-2xl flex-col mt-10 mb-20">
       <ResourceCard 
         key={selectedResource._id}
         title={selectedResource.title}
@@ -42,6 +59,11 @@ const page = async ({ params }: { params: { id: string }}) => {
         downloadNumber={selectedResource.views}
         downloadLink={selectedResource.downloadLink}
       />
+
+      <section className="mt-6 w-full flex flex-col items-start">
+          <h2 className="self-start heading2 text-white">Weather in {selectedResource.title}:</h2>
+          <WeatherComponent city={selectedResource.title} startDate={`${formattedToday}`} endDate={`${formattedNextWeek}`}/>
+        </section>
     </div>
   );
 };
